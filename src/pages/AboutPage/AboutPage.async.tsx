c4bdfeabd3855661c5610ee9f9035c4905c47efe
Oleg Demchenko <d.oleg-19953@mail.ru>
2023-10-23 15:37:18 +0300

    ability to switch themes, store settings and store colors

--- src/pages/AboutPage/AboutPage.async.tsx
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -1,3 +1,3 @@
-import { lazy } from "react";
+import React, { lazy } from "react";
 
 export const AboutPageAsync = lazy(() => import('./AboutPage'));
--- src/pages/MainPage/MainPage.async.tsx
+++ src/pages/MainPage/MainPage.async.tsx
@@ -1,3 +1,3 @@
-import { lazy } from "react";
+import React, { lazy } from "react";
 
 export const MainPageAsync = lazy(() => import('./MainPage'));
