57cc97a9bae88b3f1079fc65a76863bcd3043dbc
Oleg Demchenko <o.demchenko@iteco-inno.ru>
2024-05-01 15:59:24 +0300

    добавлен пакет react-router-dom, добавлен роутинг в приложение, декомпозиция общего бандла на чанки

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Counter } from "./components/Counter";
+import React, { Suspense } from 'react';
+import {Link, Route, Routes} from "react-router-dom";
+import { AboutPageAsync, MainPageAsync } from "./pages";
 import "./index.scss"
 
 const App = () => {
   return (
     <div className="app">
-      lorem
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export { AboutPageAsync } from "./AboutPage/AboutPage.async";
+export { MainPageAsync } from "./MainPage/MainPage.async";
